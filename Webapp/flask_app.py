6f94b83a23b986a79f4307b9a43cac55a1176b8f
Jyoti Gupta <jyotiguptahk@gmail.com>
2019-04-25 22:11:59 +0800

    test

--- Webapp/flask_app.py
+++ Webapp/flask_app.py
@@ -14,6 +14,7 @@ def index():
 def ValuePredictor(to_predict_list):
     to_predict = np.array(to_predict_list).reshape(1,12)
     loaded_model = pickle.load(open("model.pkl","rb"))
+    print(to_predict)
     result = loaded_model.predict(to_predict)
     return result[0]
 
